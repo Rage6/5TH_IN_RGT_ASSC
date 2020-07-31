b8f968e1cbc4d9efff742af58d4ba2223e3e8321
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-07-31 14:39:56 -0400

    authentication added, js and css commented out

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,23 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>5th Infantry Regiment Association</title>
-    </head>
-    <body>
-      Testing
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

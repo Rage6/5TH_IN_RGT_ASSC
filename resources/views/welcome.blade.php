84e44b24a0e21fa7abb598724151c6d1d857bb11
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-08-25 16:13:13 -0400

    removed old comments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,185 +36,182 @@
     <script src="js/my_custom/welcome/welcome.js"></script>
   </head>
   <body>
-    <!-- <div class="wholeBody"> -->
-      <div class="menuBody">
-        <div class="mainMenuTopBttn">
-          <a class="mainMenuLink" href="{{ url('/') }}">
-            <img src="images/welcome/5INF_crest-min.png"/>
-          </a>
-          <div id="mainMenuTopBttn" class="mainMenuTopSquare">
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-          <!-- <img id="mainMenuTopBttn" src="images/welcome/main_menu-min.png"/> -->
-        </div>
-        <div class="mainMenuBox">
-          <a href="{{ url('/') }}">
-            <div class="mainMenuBttn" data-bttn-num="0">
-              HOME
-            </div>
-          </a>
-          <div>
-            <div class="mainMenuBttn" data-bttn-num="1">
-              ASSOCIATION
-            </div>
-            <div class="mainMenuSubBox" data-box-num="1">
-              <div class="mainMenuSubBttn">+ What Is A Member?</div>
-              <div class="mainMenuSubBttn">+ Becoming A Member</div>
-              <div class="mainMenuSubBttn">+ Newsletter Sample</div>
-              <div class="mainMenuSubBttn">+ Merchandise Example</div>
-              <div class="mainMenuSubBttn">+ Our Donations</div>
-            </div>
-          </div>
-          <div>
-            <div class="mainMenuBttn" data-bttn-num="2">
-              HISTORY
-            </div>
-            <div class="mainMenuSubBox" data-box-num="2">
-              <div class="mainMenuSubBttn">+ Origin & Traditions</div>
-              <div class="mainMenuSubBttn">+ History Timeline</div>
-              <!-- <div class="mainMenuSubBttn">+ War of 1812</div>
-              <div class="mainMenuSubBttn">+ 1815 - 1845</div>
-              <div class="mainMenuSubBttn">+ Mexican War</div>
-              <div class="mainMenuSubBttn">+ Frontier Defense & Indian Wars</div>
-              <div class="mainMenuSubBttn">+ 1891 - 1939</div>
-              <div class="mainMenuSubBttn">+ World War II</div>
-              <div class="mainMenuSubBttn">+ Korean War</div>
-              <div class="mainMenuSubBttn">+ 1954 - 1966</div>
-              <div class="mainMenuSubBttn">+ Vietnam War</div>
-              <div class="mainMenuSubBttn">+ 1975 - 2001</div>
-              <div class="mainMenuSubBttn" style="cursor:none;background-color:grey">+ Global War On Terror</div>
-              <div class="mainMenuSubBttn" style="padding-left:15%">+ Iraq</div>
-              <div class="mainMenuSubBttn" style="padding-left:15%">+ Afghanistan</div> -->
-              <div class="mainMenuSubBttn">+ Book: "Two Centuries of Valor: The Story of the 5th Infantry Regiment"</div>
-            </div>
-          </div>
-          <div>
-            <div class="mainMenuBttn" data-bttn-num="3">
-              HALL OF HONOR
-            </div>
-            <div class="mainMenuSubBox" data-box-num="3">
-              <div class="mainMenuSubBttn">+ KIA, MIA, & Died In Service</div>
-              <div class="mainMenuSubBttn">+ Medal of Honor Recipients</div>
-              <div class="mainMenuSubBttn">+ Deceased Members</div>
-            </div>
-          </div>
-          <div>
-            <div class="mainMenuBttn" data-bttn-num="4">
-              PHOTO ALBUM
-            </div>
-            <div class="mainMenuSubBox" data-box-num="4">
-              <div class="mainMenuSubBttn">+ Album 1</div>
-              <div class="mainMenuSubBttn">+ Album 2</div>
-            </div>
-          </div>
-          @if (Route::has('login'))
-            <div>
-              <div class="mainMenuBttn" data-bttn-num="5">MEMBERS ONLY</div>
-              <div class="mainMenuSubBox" data-box-num="5">
-                  @auth
-                      <a href="{{ url('/home') }}">
-                        <div class="mainMenuSubBttn">
-                          + My Profile
-                        </div>
-                      </a>
-                      <div class="mainMenuSubBttn">+ Clothing & Items</div>
-                      <div class="mainMenuSubBttn">+ Reunions</div>
-                      <div class="mainMenuSubBttn">+ Newsletters</div>
-                  @else
-                      <a href="{{ route('login') }}">
-                        <div class="mainMenuSubBttn">+ Login</div>
-                      </a>
-                  @endauth
-              </div>
-            </div>
-          @endif
+    <div class="menuBody">
+      <div class="mainMenuTopBttn">
+        <a class="mainMenuLink" href="{{ url('/') }}">
+          <img src="images/welcome/5INF_crest-min.png"/>
+        </a>
+        <div id="mainMenuTopBttn" class="mainMenuTopSquare">
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
       </div>
-      <div class="introBody introBodyMax">
-        <div class="introBody introImage introIraq"></div>
-        <div class="introBody introImage introAfghanistan"></div>
-        <div class="introBody introImage introVietnam"></div>
-        <div class="introBody introImage introKorea"></div>
-        <div class="introBody introImage introWW2"></div>
-        <div class="introBody introImage introFrontier"></div>
-        <div class="introBody introImage intro1812"></div>
+      <div class="mainMenuBox">
+        <a href="{{ url('/') }}">
+          <div class="mainMenuBttn" data-bttn-num="0">
+            HOME
+          </div>
+        </a>
+        <div>
+          <div class="mainMenuBttn" data-bttn-num="1">
+            ASSOCIATION
+          </div>
+          <div class="mainMenuSubBox" data-box-num="1">
+            <div class="mainMenuSubBttn">+ What Is A Member?</div>
+            <div class="mainMenuSubBttn">+ Becoming A Member</div>
+            <div class="mainMenuSubBttn">+ Newsletter Sample</div>
+            <div class="mainMenuSubBttn">+ Merchandise Example</div>
+            <div class="mainMenuSubBttn">+ Our Donations</div>
+          </div>
+        </div>
+        <div>
+          <div class="mainMenuBttn" data-bttn-num="2">
+            HISTORY
+          </div>
+          <div class="mainMenuSubBox" data-box-num="2">
+            <div class="mainMenuSubBttn">+ Origin & Traditions</div>
+            <div class="mainMenuSubBttn">+ History Timeline</div>
+            <!-- <div class="mainMenuSubBttn">+ War of 1812</div>
+            <div class="mainMenuSubBttn">+ 1815 - 1845</div>
+            <div class="mainMenuSubBttn">+ Mexican War</div>
+            <div class="mainMenuSubBttn">+ Frontier Defense & Indian Wars</div>
+            <div class="mainMenuSubBttn">+ 1891 - 1939</div>
+            <div class="mainMenuSubBttn">+ World War II</div>
+            <div class="mainMenuSubBttn">+ Korean War</div>
+            <div class="mainMenuSubBttn">+ 1954 - 1966</div>
+            <div class="mainMenuSubBttn">+ Vietnam War</div>
+            <div class="mainMenuSubBttn">+ 1975 - 2001</div>
+            <div class="mainMenuSubBttn" style="cursor:none;background-color:grey">+ Global War On Terror</div>
+            <div class="mainMenuSubBttn" style="padding-left:15%">+ Iraq</div>
+            <div class="mainMenuSubBttn" style="padding-left:15%">+ Afghanistan</div> -->
+            <div class="mainMenuSubBttn">+ Book: "Two Centuries of Valor: The Story of the 5th Infantry Regiment"</div>
+          </div>
+        </div>
+        <div>
+          <div class="mainMenuBttn" data-bttn-num="3">
+            HALL OF HONOR
+          </div>
+          <div class="mainMenuSubBox" data-box-num="3">
+            <div class="mainMenuSubBttn">+ KIA, MIA, & Died In Service</div>
+            <div class="mainMenuSubBttn">+ Medal of Honor Recipients</div>
+            <div class="mainMenuSubBttn">+ Deceased Members</div>
+          </div>
+        </div>
+        <div>
+          <div class="mainMenuBttn" data-bttn-num="4">
+            PHOTO ALBUM
+          </div>
+          <div class="mainMenuSubBox" data-box-num="4">
+            <div class="mainMenuSubBttn">+ Album 1</div>
+            <div class="mainMenuSubBttn">+ Album 2</div>
+          </div>
+        </div>
+        @if (Route::has('login'))
+          <div>
+            <div class="mainMenuBttn" data-bttn-num="5">MEMBERS ONLY</div>
+            <div class="mainMenuSubBox" data-box-num="5">
+                @auth
+                    <a href="{{ url('/home') }}">
+                      <div class="mainMenuSubBttn">
+                        + My Profile
+                      </div>
+                    </a>
+                    <div class="mainMenuSubBttn">+ Clothing & Items</div>
+                    <div class="mainMenuSubBttn">+ Reunions</div>
+                    <div class="mainMenuSubBttn">+ Newsletters</div>
+                @else
+                    <a href="{{ route('login') }}">
+                      <div class="mainMenuSubBttn">+ Login</div>
+                    </a>
+                @endauth
+            </div>
+          </div>
+        @endif
       </div>
-      <div class="introBody">
-        <div class="mainTitle">
-          <div>5th Infantry</div>
-          <div>Regiment</div>
-          <div>Association</div>
-        </div>
-        <div id="bottomView" class="bottomView">
-          <div>LEARN MORE BELOW</div>
-        </div>
+    </div>
+    <div class="introBody introBodyMax">
+      <div class="introBody introImage introIraq"></div>
+      <div class="introBody introImage introAfghanistan"></div>
+      <div class="introBody introImage introVietnam"></div>
+      <div class="introBody introImage introKorea"></div>
+      <div class="introBody introImage introWW2"></div>
+      <div class="introBody introImage introFrontier"></div>
+      <div class="introBody introImage intro1812"></div>
+    </div>
+    <div class="introBody">
+      <div class="mainTitle">
+        <div>5th Infantry</div>
+        <div>Regiment</div>
+        <div>Association</div>
       </div>
-      <div class="lowerBody">
-        <div id="welcomeTitle" class="sectionTitle">Home of the Bobcats</div>
-        <div class="sectionContent welcomeContent">
-          This organization is dedicated to all of the past, present, and future members of the United States Army's <u>5th Infantry Regiment</u>, commonly known as the "Bobcats". The 5th Regiment (or its ancestral regiments) has participated in nearly every major American conflict since 1808. Its soldiers are the sinews of the Regiment, both in peace and in war. They are the fabric of its history and its tradition.
+      <div id="bottomView" class="bottomView">
+        <div>LEARN MORE BELOW</div>
+      </div>
+    </div>
+    <div class="lowerBody">
+      <div id="welcomeTitle" class="sectionTitle">Home of the Bobcats</div>
+      <div class="sectionContent welcomeContent">
+        This organization is dedicated to all of the past, present, and future members of the United States Army's <u>5th Infantry Regiment</u>, commonly known as the "Bobcats". The 5th Regiment (or its ancestral regiments) has participated in nearly every major American conflict since 1808. Its soldiers are the sinews of the Regiment, both in peace and in war. They are the fabric of its history and its tradition.
+      </div>
+      <div class="allCompartments">
+        <div class="oneCompartment desktopTitle">
+          <div class="sectionTitle pastTitle">
+            Reflect On Our Past
+          </div>
+          <div class="sectionTitle presentTitle">
+            Act In Our Present
+          </div>
+          <div class="sectionTitle futureTitle">
+            Join In Our Future
+          </div>
         </div>
-        <div class="allCompartments">
-          <div class="oneCompartment desktopTitle">
-            <div class="sectionTitle pastTitle">
+        <div class="oneCompartment">
+          <div class="mobileComponent desktopComponent">
+            <div class="sectionTitle pastTitle mobileTitle">
               Reflect On Our Past
             </div>
-            <div class="sectionTitle presentTitle">
+            <div class="sectionContent pastContent">
+              <div>
+                The 5th Regiment has a unique and exciting military history spanning over 200 years. Here you can discover the Regiment's origin, read about it's role at any given time period, and even review a book dedicated to the Regiment.
+              </div></br>
+              <div>
+                Additionally, you can find the names of every 5th Regiment soldier that recieved the Medal of Honor, every soldier that died as a result of their service, and every deceased member of the Association.
+              </div>
+            </div>
+          </div>
+          <div class="mobileComponent desktopComponent">
+            <div class="sectionTitle presentTitle mobileTitle">
               Act In Our Present
             </div>
-            <div class="sectionTitle futureTitle">
+            <div class="sectionContent presentContent">
+              The 5th Regiment Infantry Association is a very active organization. It carries out an official reunion every year, and members receive updates in the Association's regular newsletters. It also maintains frequent communications with the active Army units that currently bear our Regiment's crest. Finally, the Association often works to raise money and support for our active duty soldiers, fellow members, and related worthwhile causes.
+            </div>
+          </div>
+          <div class="mobileComponent desktopComponent">
+            <div class="sectionTitle futureTitle mobileTitle">
               Join In Our Future
             </div>
-          </div>
-          <div class="oneCompartment">
-            <div class="mobileComponent desktopComponent">
-              <div class="sectionTitle pastTitle mobileTitle">
-                Reflect On Our Past
-              </div>
-              <div class="sectionContent pastContent">
-                <div>
-                  The 5th Regiment has a unique and exciting military history spanning over 200 years. Here you can discover the Regiment's origin, read about it's role at any given time period, and even review a book dedicated to the Regiment.
-                </div></br>
-                <div>
-                  Additionally, you can find the names of every 5th Regiment soldier that recieved the Medal of Honor, every soldier that died as a result of their service, and every deceased member of the Association.
-                </div>
-              </div>
-            </div>
-            <div class="mobileComponent desktopComponent">
-              <div class="sectionTitle presentTitle mobileTitle">
-                Act In Our Present
-              </div>
-              <div class="sectionContent presentContent">
-                The 5th Regiment Infantry Association is a very active organization. It carries out an official reunion every year, and members receive updates in the Association's regular newsletters. It also maintains frequent communications with the active Army units that currently bear our Regiment's crest. Finally, the Association often works to raise money and support for our active duty soldiers, fellow members, and related worthwhile causes.
-              </div>
-            </div>
-            <div class="mobileComponent desktopComponent">
-              <div class="sectionTitle futureTitle mobileTitle">
-                Join In Our Future
-              </div>
-              <div class="sectionContent futureContent">
-                <div>
-                  The 5th Infantry Regiment Association's door is always open to every honorable soldier that has served as part of the 5th Infantry Regiment, be they past or present. It also accepts "associate members", meaning any family member of a Bobcat veteran. Members can update their personal profile, communicate with other members easier, read any newsletter, and purchase members-only merchandise.
-                </div>
+            <div class="sectionContent futureContent">
+              <div>
+                The 5th Infantry Regiment Association's door is always open to every honorable soldier that has served as part of the 5th Infantry Regiment, be they past or present. It also accepts "associate members", meaning any family member of a Bobcat veteran. Members can update their personal profile, communicate with other members easier, read any newsletter, and purchase members-only merchandise.
               </div>
             </div>
           </div>
-        </div>
-        <div class="footer">
-          <a href="{{ url('/') }}">
-            <div>HOME</div>
-          </a>
-          <a>
-            <div>ASSOCIATION</div>
-          </a>
-          <div>HISTORY</div>
-          <div>HALL OF HONOR</div>
-          <div>PHOTO ALBUM</div>
-          <div>MEMBERS ONLY</div>
         </div>
       </div>
-    <!-- </div> -->
+      <div class="footer">
+        <a href="{{ url('/') }}">
+          <div>HOME</div>
+        </a>
+        <a>
+          <div>ASSOCIATION</div>
+        </a>
+        <div>HISTORY</div>
+        <div>HALL OF HONOR</div>
+        <div>PHOTO ALBUM</div>
+        <div>MEMBERS ONLY</div>
+      </div>
+    </div>
   </body>
 </html>

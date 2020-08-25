b26ddf5e8327b658b5765ae0fe858b321d681ce1
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-08-25 16:49:56 -0400

    moved menu css to seperate folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,19 +14,24 @@
     <link href="https://fonts.googleapis.com/css2?family=Allerta+Stencil&family=Bellefair&family=Bellota+Text&family=Black+Ops+One&family=Charm&family=Cinzel&family=IM+Fell+English+SC&family=Italianno&family=Nanum+Myeongjo&family=News+Cycle&family=Prompt&family=Special+Elite&family=Spectral+SC&family=Staatliches&family=Stardos+Stencil&family=Thasadith&family=Vollkorn+SC&display=swap" rel="stylesheet">
 
     <!-- Styles -->
-    <!-- Default CSS file; 0px -> 360px-->
+    <!-- Default CSS files; 0px -> 360px-->
+    <link rel="stylesheet" type="text/css" href="css/my_custom/menu/360_menu.css">
     <link rel="stylesheet" type="text/css" href="css/my_custom/welcome/360_welcome.css">
     <!-- 361px -> 375px-->
     <link rel="stylesheet" media="screen and (min-width: 361px) and (max-width: 375px)" type="text/css" href="css/my_custom/welcome/375_welcome.css">
     <!-- 376px -> 414px-->
     <link rel="stylesheet" media="screen and (min-width: 375px) and (max-width: 414px)" type="text/css" href="css/my_custom/welcome/414_welcome.css">
     <!-- 415px -> 768px-->
+    <link rel="stylesheet" media="screen and (min-width: 415px) and (max-width: 768px)" type="text/css" href="css/my_custom/menu/768_menu.css">
     <link rel="stylesheet" media="screen and (min-width: 415px) and (max-width: 768px)" type="text/css" href="css/my_custom/welcome/768_welcome.css">
     <!-- 769px -> 1366px-->
+    <link rel="stylesheet" media="screen and (min-width: 769px) and (max-width: 1366px)" type="text/css" href="css/my_custom/menu/1366_menu.css">
     <link rel="stylesheet" media="screen and (min-width: 769px) and (max-width: 1366px)" type="text/css" href="css/my_custom/welcome/1366_welcome.css">
     <!-- 1367px -> 1920px-->
+    <link rel="stylesheet" media="screen and (min-width: 1367px) and (max-width: 1920px)" type="text/css" href="css/my_custom/menu/1920_menu.css">
     <link rel="stylesheet" media="screen and (min-width: 1367px) and (max-width: 1920px)" type="text/css" href="css/my_custom/welcome/1920_welcome.css">
     <!-- 1921px and Greater -->
+    <link rel="stylesheet" media="screen and (min-width: 1921px)" type="text/css" href="css/my_custom/menu/past_1920_menu.css">
     <link rel="stylesheet" media="screen and (min-width: 1921px)" type="text/css" href="css/my_custom/welcome/past_1920_welcome.css">
 
     <link rel="icon" href="images/welcome/5INF_crest-min.png">

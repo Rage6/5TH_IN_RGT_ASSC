d66a17d8aca5d57560cef902ea1fbbdc5b4b6a17
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-09-18 16:38:13 -0400

    swapping .outerWidth with .screen.width / devicePixelRatio

--- public/js/my_custom/menu/menu.js
+++ public/js/my_custom/menu/menu.js
@@ -1,23 +1,38 @@
 $(document).ready(() => {
   // console.log("Testing");
-  
+  console.log("screen width: " + window.screen.width);
+  console.log("screen height: " + window.screen.height);
+  console.log("device pixel ration: " + window.devicePixelRatio);
+  console.log(window);
+
   $('#mainMenuTopBttn').click(()=>{
+    // console.log(window.outerWidth);
+    // console.log(window.screen.width / window.devicePixelRatio);
+    // console.log(window.devicePixelRatio);
+
     let windowWidth = window.outerWidth + "px";
+    // let windowWidth = window.outerWidth / window.devicePixelRatio + "px";
     let topBttnHeight = $('.mainMenuTopBttn').outerHeight(true);
-    if (window.outerWidth <= 1920) {
+    // if (window.outerWidth <= 1920) {
+    if (window.screen.width / window.devicePixelRatio <= 1920) {
       if ($('.mainMenuBox').css('left') != windowWidth) {
-        if (window.outerWidth <= 768) {
+        // if (window.outerWidth <= 768) {
+        if (window.screen.width / window.devicePixelRatio <= 768) {
           $('.mainMenuBox').css('animation-name','scrollMenuAllLeft').css('top',topBttnHeight);
-        } else if (window.outerWidth >= 768 && window.outerWidth <= 1366) {
+        // } else if (window.outerWidth >= 768 && window.outerWidth <= 1366) {
+        } else if (window.screen.width / window.devicePixelRatio >= 768 && window.screen.width / window.devicePixelRatio <= 1366) {
           $('.mainMenuBox').css('animation-name','scrollMenuHalfLeft').css('top',topBttnHeight);
-        } else if (window.outerWidth > 1366 && window.outerWidth <= 1920) {
+        // } else if (window.outerWidth > 1366 && window.outerWidth <= 1920) {
+        } else if (window.screen.width / window.devicePixelRatio > 1366 && window.screen.width / window.devicePixelRatio <= 1920) {
           $('.mainMenuBox').css('animation-name','scrollMenuThirdLeft').css('top',topBttnHeight);
         };
         $('.mainMenuTopBttn').css('background','linear-gradient(rgba(0,100,0,0.8),rgba(0,100,0,0.8) 85%,rgba(0,0,0,0.8))');
       } else {
-        if (window.outerWidth <= 768) {
+        // if (window.outerWidth <= 768) {
+        if (window.screen.width / window.devicePixelRatio <= 768) {
           $('.mainMenuBox').css('animation-name','scrollMenuAllRight').css('top',topBttnHeight);
-        } else if (window.outerWidth >= 768 && window.outerWidth <= 1366) {
+        // } else if (window.outerWidth >= 768 && window.outerWidth <= 1366) {
+        } else if (window.screen.width / window.devicePixelRatio >= 768 && window.screen.width / window.devicePixelRatio <= 1366) {
           $('.mainMenuBox').css('animation-name','scrollMenuHalfRight').css('top',topBttnHeight);
         } else {
           $('.mainMenuBox').css('animation-name','scrollMenuThirdRight').css('top',topBttnHeight);
@@ -26,7 +41,8 @@ $(document).ready(() => {
       };
     } else {
       console.log($('.mainMenuTopBttn').css('left'));
-      let newLeft = (window.outerWidth / 2) + 320;
+      // let newLeft = (window.outerWidth / 2) + 320;
+      let newLeft = ((window.screen.width / window.devicePixelRatio) / 2) + 320;
       let newTop = $('.mainMenuTopBttn').outerHeight(true);
       if ($('.mainMenuBox').css('display') == 'block') {
         $('.mainMenuBox').css('display','none').css('left',newLeft).css('top',newTop);

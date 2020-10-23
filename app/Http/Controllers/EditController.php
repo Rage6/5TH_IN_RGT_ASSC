e9a086e1e5bccb3eedf5c663d01540f542ec1dd8
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-10-23 12:26:56 -0400

    bio can be updated now

--- app/Http/Controllers/EditController.php
+++ app/Http/Controllers/EditController.php
@@ -6,7 +6,8 @@ namespace App\Http\Controllers;
 use App\User;
 // use Illuminate\Http\Input;
 // use Illuminate\Http\Request;
-use Request;
+// use Request;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
 
@@ -33,11 +34,12 @@ class EditController extends Controller
         return view('edit',compact('this_user'));
     }
 
-    public function updateBasicInfo()
+    public function updateBasicInfo(Request $request)
     {
       $this_user = Auth::user();
       $this_user->first_name = Request::input('first_name');
       $this_user->last_name = Request::input('last_name');
+      $this_user->biography = Request::input('biography');
       $this_user->save();
       // $request->session()->put('status','Update Successful');
       return redirect('home/edit');
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -26,8 +26,12 @@
                         <input name="last_name" id="lastName" placeholder="Last Name" value="{{ $this_user->last_name }}" />
                       </div>
                       <div class="form-group">
-                        <label for="bio">Personal History</label>
-                        <textarea class="form-control" id="bio" name="bio_box" placeholder="Provide a brief summary of yourself and your time in the 5th" value="{{ $this_user->last_name }}"></textarea>
+                        <label for="email">Email</label>
+                        <input name="email" id="email" placeholder="Last Name" value="{{ $this_user->email }}" />
+                      </div>
+                      <div class="form-group">
+                        <label for="biography">Personal History</label>
+                        <textarea class="form-control" id="biography" name="biography" maxlength="255" placeholder="Provide a brief summary of yourself and your time in the 5th">{{ $this_user->biography }}</textarea>
                       </div>
                       <button type="submit" name="updateBasicInfo" class="btn btn-primary">
                         UPDATE CHANGES

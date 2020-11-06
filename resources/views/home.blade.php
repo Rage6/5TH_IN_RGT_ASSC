9c18d39460a45bc52d5c8ee10b1f443d6e7a996c
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-11-06 15:49:06 -0500

    started custom CSS for home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 
-@section('content')
+@section('home_style')
+  <!-- Home Styles -->
+  <!-- Default CSS files; 0px -> 360px-->
+  <link rel="stylesheet" type="text/css" href="/css/my_custom/home/360_home.css">
+  <!-- 361px -> 375px-->
+  <link rel="stylesheet" media="screen and (min-width: 361px) and (max-width: 375px)" type="text/css" href="/css/my_custom/home/375_home.css">
+  <!-- 376px -> 414px-->
+  <link rel="stylesheet" media="screen and (min-width: 375px) and (max-width: 414px)" type="text/css" href="/css/my_custom/home/414_home.css">
+  <!-- 415px -> 768px-->
+  <link rel="stylesheet" media="screen and (min-width: 415px) and (max-width: 768px)" type="text/css" href="/css/my_custom/home/768_home.css">
+  <!-- 769px -> 1366px-->
+  <link rel="stylesheet" media="screen and (min-width: 769px) and (max-width: 1366px)" type="text/css" href="/css/my_custom/home/1366_home.css">
+  <!-- 1367px -> 1920px-->
+  <link rel="stylesheet" media="screen and (min-width: 1367px) and (max-width: 1920px)" type="text/css" href="/css/my_custom/home/1920_home.css">
+  <!-- 1921px and Greater -->
+  <link rel="stylesheet" media="screen and (min-width: 1921px)" type="text/css" href="/css/my_custom/home/past_1920_home.css">
+@stop
+
+@section('home_content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -20,12 +38,18 @@
                         </div>
                     @endif
                     <div class="card">
-                      @if ($this_user->current_img)
-                        <!-- <img class="card-img-top" src='{{ $this_user->current_img }}?t=<?php echo(time()); ?>'/> -->
-                        <img class="img-thumbnail" src="https://5th-rgt-profile-photos.s3.us-east-2.amazonaws.com/{{ auth()->user()->current_img }}?t=<?php echo(time()); ?>">
-                      @else
-                        <img class="card-img-top" src="/uploads/images/profile-default.jpg">
-                      @endif
+                      <div style="display:flex;flex-wrap: wrap">
+                        @if ($this_user->current_img)
+                          <div style="height:200px;width:50%;background-image: url('https://5th-rgt-profile-photos.s3.us-east-2.amazonaws.com/{{ auth()->user()->current_img }}?t=<?php echo(time()); ?>');background-size:cover;background-position:center"></div>
+                        @else
+                          <div style="height:200px;width:50%;background-image: url('/uploads/images/profile-default.jpg');background-size:cover;background-position:center"></div>
+                        @endif
+                        @if ($this_user->veteran_img)
+                          <div style="height:200px;width:50%;background-image: url('https://5th-rgt-profile-photos.s3.us-east-2.amazonaws.com/{{ auth()->user()->veteran_img }}?t=<?php echo(time()); ?>');background-size:cover;background-position:center"></div>
+                        @else
+                          <div style="height:200px;width:50%;background-image: url('/uploads/images/profile-default.jpg');background-size:cover;background-position:center"></div>
+                        @endif
+                      </div>
                       <div class="card-body">
                         <h5 class="card-title">
                           {{ $this_user->first_name }} {{ $this_user->last_name }}

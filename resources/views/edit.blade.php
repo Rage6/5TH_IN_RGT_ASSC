31cc8a6d7f28be12f8a3f60350384bbc7850d530
Nicholas Vogt <nicholas.vogt2017@gmail.com>
2021-01-22 15:46:07 -0500

    cleaned up Edit page

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -17,36 +17,36 @@
                     <!-- {{ __('This is the edit page') }} -->
                     <form method="POST" action="edit/basics">
                       @csrf
-                      <div class="form-group">
-                        <label for="firstName">First Name</label>
+                      <div class="basicInfoGrid">
+                        <div>First Name</div>
                         <input name="first_name" id="firstName" placeholder="First Name" value="{{ $this_user->first_name }}" required />
                       </div>
-                      <div class="form-group">
-                        <label for="lastName">Last Name</label>
+                      <div class="basicInfoGrid">
+                        <div>Last Name</div>
                         <input name="last_name" id="lastName" placeholder="Last Name" value="{{ $this_user->last_name }}" required />
                       </div>
-                      <div class="form-group">
-                        <label for="email">Email</label>
+                      <div class="basicInfoGrid">
+                        <div>Email</div>
                         <input type="email" name="email" id="email" placeholder="Email" value="{{ $this_user->email }}" required/>
                       </div>
-                      <div class="row">
+                      <div class="imgGrid">
                       @if (auth()->user()->current_img)
-                        <!-- <div class="img-thumbnail rounded float-left w-50" style="background-image: url('{{ auth()->user()->current_img }}?t=<?php echo(time()); ?>'); background-size:cover; background-position: center; min-width: 100px; min-height: 300px"></div> -->
-                        <div class="img-thumbnail rounded float-left w-50" style="background-image: url('https://5th-rgt-profile-photos.s3.us-east-2.amazonaws.com/{{ auth()->user()->current_img }}?t=<?php echo(time()); ?>'); background-size:cover; background-position: center; min-width: 100px; min-height: 300px"></div>
+                        <div
+                          class="img-thumbnail rounded"
+                          style="background-image: url('https://5th-rgt-profile-photos.s3.us-east-2.amazonaws.com/{{ auth()->user()->current_img }}?t=<?php echo(time()); ?>'); background-size:contain; background-position: center; background-repeat:no-repeat"></div>
                       @else
-                        <img class="img-thumbnail rounded float-right w-50" src="/uploads/images/profile-default.jpg">
+                        <img class="img-thumbnail rounded" src="/uploads/images/profile-default.jpg">
                       @endif
                       @if (auth()->user()->veteran_img)
-                        <!-- <div class="img-thumbnail rounded float-right w-50" style="background-image: url('{{ auth()->user()->veteran_img }}?t=<?php echo(time()); ?>'); background-size:cover; background-position: center; min-width: 100px; min-height: 300px"></div> -->
-                        <div class="img-thumbnail rounded float-left w-50" style="background-image: url('https://5th-rgt-profile-photos.s3.us-east-2.amazonaws.com/{{ auth()->user()->veteran_img }}?t=<?php echo(time()); ?>'); background-size:cover; background-position: center; min-width: 100px; min-height: 300px"></div>
+                        <div class="img-thumbnail rounded" style="background-image: url('https://5th-rgt-profile-photos.s3.us-east-2.amazonaws.com/{{ auth()->user()->veteran_img }}?t=<?php echo(time()); ?>'); background-size:contain; background-position: center; background-repeat:no-repeat"></div>
                       @else
-                        <img class="img-thumbnail rounded w-50" src="/uploads/images/profile-default.jpg">
+                        <img class="img-thumbnail rounded" src="/uploads/images/profile-default.jpg">
                       @endif
                       </div>
                       <button class="btn">
                         <a href="{{ route('profile') }}">{{ __('CHANGES PHOTOS') }}</a>
                       </button>
-                      <div class="form-group">
+                      <div class="form-group historyBox">
                         <label for="biography">Personal History</label>
                         <textarea class="form-control" id="biography" name="biography" maxlength="255" placeholder="Provide a brief summary of yourself and your time in the 5th">{{ $this_user->biography }}</textarea>
                       </div>

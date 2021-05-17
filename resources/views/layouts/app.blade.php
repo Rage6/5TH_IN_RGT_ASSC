dc9bbe1d0c6cfff0fab35720cca418b3485dae4d
Nicholas Vogt <nicholas.vogt2017@gmail.com>
2021-05-17 11:46:36 -0400

    cleaned up home style, ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', '5th Infantry Regiment Assoc.') }}
+                    <!-- {{ config('app.name', '5th Infantry Regiment Assoc.') }} -->
+                    <img src="/images/welcome/5INF_crest-min.png">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>

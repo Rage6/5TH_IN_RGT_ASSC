e78ccbebb36871066838b4679a49fa1617d15c6a
Nicholas Vogt <nicholas.vogt2017@gmail.com>
2022-02-12 15:11:53 -0500

    Wednesday reunion update

--- resources/views/reunion_registration.blade.php
+++ resources/views/reunion_registration.blade.php
@@ -643,7 +643,7 @@
               Wednesday - Olympic Museum, Garden of the Gods, and Lunch
             </div>
             <div class="boxSubcontent">
-              <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+              <!-- <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
                 <input type="hidden" name="cmd" value="_s-xclick">
                 <input type="hidden" name="hosted_button_id" value="9PD5E67DSBXC6">
                 <input type="hidden" name="shopping_url" value="http://fifth-infantry-regiment-assoc.herokuapp.com/reunion">
@@ -676,6 +676,29 @@
                         </option>
                         <option value="Gateway Rock Grill Out">
                           Gateway Rock Grill Out
+                        </option>
+                      </select>
+                    </td>
+                  </tr>
+                </table>
+                <input type="hidden" name="currency_code" value="USD">
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+              </form> -->
+              <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                <input type="hidden" name="cmd" value="_s-xclick">
+                <input type="hidden" name="hosted_button_id" value="9PD5E67DSBXC6">
+                <table>
+                  <tr>
+                    <td>
+                      <input type="hidden" name="on0" value="Tour Selections">Tour Selections
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <select name="os0">
+                        <option value="Olympic & Garden of Gods">
+                          Olympic & Garden of Gods $79.00 USD
                         </option>
                       </select>
                     </td>
